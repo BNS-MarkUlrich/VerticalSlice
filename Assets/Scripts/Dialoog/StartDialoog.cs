66c92243d4df51515a0a6bcf8d968fe445a49ad8
Minoord <minoord18@outlook.com>
2021-11-25 12:53:21 +0100

    Added TriggerDialoog.cs and activating Dialoog scripts

---
+++ Assets/Scripts/Dialoog/StartDialoog.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartDialoog : MonoBehaviour
+{
+    [SerializeField]private int _dialogueTimer;
+    [SerializeField] private string _dialogue;
+    [SerializeField] private DialoogTrigger _dialoogTrigger;
+    [SerializeField] private SwapperDialoog _swapperDialoog;
+
+    private bool _porR = true;
+
+    private void Start()
+    {
+        _dialoogTrigger.StartDialogue(_dialogue);        
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            _swapperDialoog.SwapPokemonDia(_porR);
+        }
+    }
+
+}
